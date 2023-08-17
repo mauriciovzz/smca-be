ab207957708874e652e257d1d9be35318750c805
mauriciovzz <mauriciovr12@gmail.com>
2023-08-17 19:54:09 -0400

    MQTT support

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -6,7 +7,20 @@ const locationsRouter = require('./controllers/locations')
 const valuesRouter = require('./controllers/values')
 const variablesRouter = require('./controllers/variables')
 const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
+
+const mqtt = require('mqtt')
+const client = mqtt.connect()
+var options = {
+    host: config.MQTT_HOST,
+    port: config.MQTT_PORT,
+    protocol: config.MQTT_PROTOCOL,
+    username: config.MQTT_USERNAME,
+    password: config.MQTT_PASSWORD
+}
+client.subscribe('#')
+client.on('message', (topic, message) => {
+    console.log('Received message:', topic, message.toString());
+});
 
 app.use(cors())
 app.use(express.static('dist'))
--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,18 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const DATABASE_URL = process.env.DATABASE_URL
 
+const MQTT_HOST = process.env.MQTT_HOST
+const MQTT_PORT = process.env.MQTT_PORT
+const MQTT_PROTOCOL = process.env.MQTT_PROTOCOL
+const MQTT_USERNAME = process.env.MQTT_USERNAME
+const MQTT_PASSWORD = process.env.MQTT_PASSWORD
+
 module.exports = {
     DATABASE_URL,
-    PORT
+    PORT,
+    MQTT_HOST,
+    MQTT_PORT,
+    MQTT_PROTOCOL,
+    MQTT_USERNAME,
+    MQTT_PASSWORD
 }
